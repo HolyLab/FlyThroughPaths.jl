16cd857d570bfe2fcaf2a9fa9cc6ae57f0a0db3e
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-05-19 22:19:07 +0530

    Remove outdated comments

--- ext/FlyThroughPathsMakieExt.jl
+++ ext/FlyThroughPathsMakieExt.jl
@@ -5,7 +5,6 @@ using Makie
 
 function FlyThroughPaths.capture_view(cam::Camera3D)
     view = ViewState(eyeposition = cam.eyeposition[], lookat = cam.lookat[], fov = cam.fov[], upvector = cam.upvector[])
-    # TODO: extract fov and upvector from cam
     return view
 end
 FlyThroughPaths.capture_view(scene::Scene) = capture_view(cameracontrols(scene))
@@ -16,7 +15,7 @@ function FlyThroughPaths.set_view!(scene::Scene, view::ViewState)
     cameracontrols(scene).fov[] = view.fov[]
     return scene
 end
-# FlyThroughPaths.set_view!(scene::Scene, view::ViewState) = set_view!(cameracontrols(scene), view)
+
 FlyThroughPaths.set_view!(axis::Makie.AbstractAxis, view::ViewState) = set_view!(axis.scene, view)
 
 end
