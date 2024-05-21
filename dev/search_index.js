fcbe98fecdc3fb87e9f5a3f44983e468c1eed7e1
Documenter.jl <documenter@juliadocs.github.io>
2024-05-21 10:36:38 +0000

    build based on 64e0814

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FlyThroughPaths","category":"page"},{"location":"#FlyThroughPaths","page":"Home","title":"FlyThroughPaths","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FlyThroughPaths.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FlyThroughPaths]","category":"page"}]
+}
