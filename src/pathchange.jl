432b8c0cb9add0687371a6c51b496f1c025c5296
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-05-28 13:24:34 -0400

    Add more docstrings + document functions not structs

--- src/pathchange.jl
+++ src/pathchange.jl
@@ -23,11 +23,6 @@ an `action` callback.
 """
 abstract type PathChange{T<:Real} end
 
-"""
-    Pause(duration, [action])
-
-Pause at the current position for `duration`.
-"""
 struct Pause{T} <: PathChange{T}
     duration::T
     action
@@ -37,6 +32,12 @@ struct Pause{T} <: PathChange{T}
         new{T}(t, action)
     end
 end
+
+"""
+    Pause(duration, [action])
+
+Pause at the current position for `duration`.
+"""
 Pause(duration::T) where T = Pause{T}(duration)
 
 Base.convert(::Type{Pause{T}}, p::Pause) where T = Pause{T}(p.duration, p.action)
@@ -48,7 +49,6 @@ struct ConstrainedMove{T} <: PathChange{T}
     constraint::Symbol
     speed::Symbol
     action
-
     function ConstrainedMove{T}(t, target, constraint, speed, action=nothing) where T
         t >= zero(T) || throw(ArgumentError("t must be non-negative"))
         constraint in (:none,:rotation) || throw(ArgumentError("Unknown constraint: $constraint"))
@@ -56,6 +56,34 @@ struct ConstrainedMove{T} <: PathChange{T}
         new{T}(t, target, constraint, speed, action)
     end
 end
+
+"""
+    ConstrainedMove(duration::T, target::ViewState{T}, [constraint, speed, action]) where T <: Real
+
+Create a `ConstrainedMove` which represents a movement from the current 
+[`ViewState`](@ref) to a `target` [`ViewState`](@ref) over a specified 
+`duration`.  The movement can be constrained by `:rotation` or 
+unconstrained by `:none`, and can proceed at a `:constant` or `:sinusoidal` 
+speed.
+
+# Arguments
+- `duration::T`: The duration of the movement, where `T` is a subtype of `Real`.
+- `target::ViewState{T}`: The target state to reach at the end of the movement.
+- `constraint::Symbol`: The type of constraint on the movement (`:none` or `:rotation`).
+- `speed::Symbol`: The speed pattern of the movement (`:constant` or `:sinusoidal`).
+- `action`: An optional callback to be called at each step of the movement.
+
+
+# Examples
+```julia
+# Move to a new view state over 5 seconds with no rotation and constant speed
+move = ConstrainedMove(5.0, new_view_state, :none, :constant)
+path *= move
+```
+
+This type of `PathChange` is useful for animations where the view needs to 
+transition smoothly between two states under certain constraints.
+"""
 ConstrainedMove{T}(duration, target; constraint=:none, speed=:constant, action=nothing) where T =
     ConstrainedMove{T}(duration, target, constraint, speed, action)
 ConstrainedMove(duration, target::ViewState{T}, args...) where T = ConstrainedMove{T}(duration, target, args...)
@@ -75,6 +103,30 @@ struct BezierMove{T} <: PathChange{T}
         new{T}(t, target, controls, action)
     end
 end
+
+"""
+    BezierMove(duration::T, target::ViewState{T}, controls::Vector{ViewState{T}}, [action]) where T <: Real
+
+Create a `BezierMove` which represents a movement from the current 
+[`ViewState`](@ref) to a `target` [`ViewState`](@ref) over a specified 
+`duration`.  The movement is defined by a series of control points, which 
+are interpolated between to form a smooth curve.  
+
+See the [Wikipedia article on Bezier curves](https://en.wikipedia.org/wiki/B%C3%A9zier_curve) for more details.
+
+# Arguments
+- `duration::T`: The duration of the movement, where `T` is a subtype of `Real`.
+- `target::ViewState{T}`: The target state to reach at the end of the movement.
+- `controls::Vector{ViewState{T}}`: The control points of the movement.
+- `action`: An optional callback to be called at each step of the movement.
+
+# Examples
+```julia
+# Move to a new view state over 5 seconds with no rotation and constant speed
+move = BezierMove(5.0, new_view_state, [new_view_state])
+path *= move
+```
+"""
 BezierMove(duration, target::ViewState{R}, controls::Vector{ViewState{S}}, args...) where {R,S} = BezierMove{promote_type(R,S)}(duration, target, controls, args...)
 
 Base.convert(::Type{BezierMove{T}}, m::BezierMove) where T = BezierMove{T}(m.duration, m.target, m.controls, m.action)
